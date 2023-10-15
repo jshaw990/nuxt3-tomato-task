906397a675fbaa9b842e516439237d68593644d3
Jayden <jayden.shaw@gmail.com>
2023-10-15 01:18:23 -0600

    coming soon

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,11 @@
 </script>
 
 <template>
-    <div>
-        Index page
-        <TimerContainer />
+    <div class="min-h-screen min-w-full flex flex-col text-center mt-32 gap-16">
+        <div class='text-4xl'>Coming soon!</div>
+        <NuxtLink to="https://jaydenshaw.com/" :external="true">
+            <Icon name='mdi:less-than-or-equal' size="32px" />
+            Jayden Shaw
+        </NuxtLink>
     </div>
 </template>
