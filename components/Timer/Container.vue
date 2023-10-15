d7837d9aaaa99f6b2ad35b915977dbc637c92a2c
Jayden <jayden.shaw@gmail.com>
2023-10-15 00:29:18 -0600

    Updated timer component

--- components/Timer/Container.vue
+++ components/Timer/Container.vue
@@ -1,36 +1,107 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
-import { NButton, NProgress } from 'naive-ui'
+import { computed, reactive } from 'vue'
+import { NButton, NButtonGroup, NProgress } from 'naive-ui'
 
 const state = reactive({
+    currentTimeInSeconds: 600,
+    isProcessing: false,
     isRunning: false,
-    percentage: 80
+    startTimeInSeconds: 600
 })
 
 const activateTimer = () => {
-    if (state.percentage > 0 && state.isRunning) {
-        if (state.percentage === 0) {
-            state.isRunning = false
-            return
-        }
-        setTimeout(() => {
-            state.percentage = state.percentage - 1
-            console.log('timer at ', state.percentage)
+    setTimeout(() => {
+        if (state.currentTimeInSeconds > 0 && state.isRunning) {
+            state.currentTimeInSeconds = state.currentTimeInSeconds - 1
+
+            if (state.currentTimeInSeconds === 0 || !state.isRunning) {
+                state.isRunning = false
+                return
+            }
+
+            if (state.currentTimeInSeconds % 15 === 0) {
+                logTimerToStorage()
+            }
+
             activateTimer()
-        }, 1000)
-    }
+        }
+    }, 1000)
 }
 
-const startTimer = () => {
-    state.isRunning = true
-    console.log('timer started')
-    activateTimer()
+const logTimerToStorage = () => {
+    const log = state
+
+    console.log(log)
+}
+
+const secondsToMinutesAndSeconds = computed((): string => {
+    const seconds = state.currentTimeInSeconds
+    const minutes: number = Math.floor(seconds / 60)  // Get the whole number of minutes
+    const remainingSeconds: number = seconds % 60    // Get the remaining seconds
+
+    // Format the result as "minutes:seconds"
+    const timeFormat: string = `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`
+
+    return timeFormat
+})
+
+const determinePercentageOfTimer = computed(() => {
+    return (state.currentTimeInSeconds / state.startTimeInSeconds) * 100
+})
+
+const resetTimer = () => {
+    if (state.isProcessing) return
+
+    state.isProcessing = true
+    state.isRunning = false
+    state.currentTimeInSeconds = state.startTimeInSeconds
+    logTimerToStorage()
+    state.isProcessing = false
+}
+
+const toggleTimerState = () => {
+    state.isProcessing = true
+    const timeAtToggle = state.currentTimeInSeconds
+    state.isRunning = !state.isRunning
+    if (state.isRunning) activateTimer()
+    if (!state.isRunning) state.currentTimeInSeconds = timeAtToggle
+    logTimerToStorage()
+    state.isProcessing = false
 }
 </script>
 
 <template>
     <div class="flex flex-col items-center gap-8">
-        <n-progress type="circle" :percentage="state.percentage" />
-        <n-button type="primary" ghost @click="startTimer">Start</n-button>
+        <NProgress :height="300" :processing="state.isRunning" type="line" :percentage="determinePercentageOfTimer"
+            style="width: 300px !important;">
+            <div class="text-center">
+                <div class="-mt-8 pb-4 text-4xl">{{ secondsToMinutesAndSeconds }}</div>
+                <NButtonGroup>
+                    <NButton round ghost class="w-[120px]" @click="toggleTimerState">
+                        {{ state.isRunning ? 'Stop timer' : 'Start timer' }}
+                    </NButton>
+                    <NButton round ghost class="w-[120px]" @click="resetTimer">
+                        Reset timer
+                    </NButton>
+                </NButtonGroup>
+            </div>
+        </NProgress>
+        <NButtonGroup>
+            <NButton round ghost class="w-[150px]">
+                <template #icon>
+                    <Icon name="ph:coffee-bold" />
+                </template>
+                Long break
+            </NButton>
+            <NButton round ghost class="w-[70px] md:w-[150px]">
+                Work
+            </NButton>
+            <NButton round ghost class="w-[150px]" icon-placement="right">
+                Short break
+                <template #icon>
+                    <Icon name="ph:coffee-bold" />
+                </template>
+            </NButton>
+        </NButtonGroup>
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,29 @@
 export default defineNuxtConfig({
-	app: {
-		head: {
-			link: [
-				{
-					rel: 'preconnect', href: 'https://fonts.googleapis.com'
-				},
-				{
-					rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous'
-				},
-				{
-					rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:wght@200;400;700;900&display=swap'
-				}
-			]
-		}
-	},
-	css: [
-		'@/assets/styles.scss'
-	],
-	modules: [
-		'@nuxtjs/device',
-		'@nuxtjs/eslint-module',
-		'@nuxtjs/tailwindcss',
-		'nuxt-icon'
-	]
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'preconnect', href: 'https://fonts.googleapis.com'
+                },
+                {
+                    rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous'
+                },
+                {
+                    rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:wght@200;400;700;900&display=swap'
+                }
+            ]
+        }
+    },
+    css: [
+        '@/assets/styles.scss'
+    ],
+    modules: [
+        '@nuxtjs/device',
+        '@nuxtjs/eslint-module',
+        '@nuxtjs/tailwindcss',
+        'nuxt-icon'
+    ],
+    eslint: {
+        fix: true
+    }
 })
