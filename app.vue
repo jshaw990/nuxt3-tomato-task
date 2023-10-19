1695f6d669e8b162d827ae6ba7de0fda167e9679
Jayden <jayden.shaw@gmail.com>
2023-10-18 23:25:43 -0600

    Setting env to prod

--- app.vue
+++ app.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { LayoutKey } from '#build/types/layouts'
-import { useRuntimeConfig } from 'nuxt/app'
+// import { useRuntimeConfig } from 'nuxt/app'
 import { computed } from 'vue'
 
-const { public: { currentEnv } } = useRuntimeConfig()
+// const { public: { currentEnv } } = useRuntimeConfig()
 
 const layoutName = computed((): LayoutKey => {
-    return currentEnv === 'development' ? 'default' : 'construction'
+    // return currentEnv === 'development' ? 'default' : 'construction'
+    return 'default'
 })
 </script>
 
